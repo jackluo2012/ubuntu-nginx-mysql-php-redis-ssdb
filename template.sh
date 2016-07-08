70eec95b6192465893c0a1d89d7f739a4451d190
jackluo <net.webjoy@gmail.com>
2016-07-08 16:40:53 +0800

    update temaplte namegit add template.sh

--- template.sh
+++ template.sh
@@ -1,46 +1,72 @@
 #!/usr/bin/env bash
 
+if [ $# != 1 ] ; then 
+echo " 请输入一个创建的网站目录" 
+exit 1; 
+fi 
+webdir=$1
+
 #mkdir -vp  test/{data,log,www,config}
-mkdir -p test/log
-mkdir -p test/www
-mkdir -p test/config
-mkdir -p test/data
-touch test/config/www.conf
-default=`cat << EOF
-server {\n
-\t    listen 80 default_server;\n
-\t    listen [::]:80 default_server ipv6only=on;\n
-\n
-\t    root /var/multrix;\n
-\t    index index.html index.htm index.php;\n
-\n
-\t    # Make site accessible from http://localhost/
-\t    server_name localhost;\n
-\n
-\t    location / {\n
-\t        # 如果找不到真实存在的文件，把请求分发至 index.php
-\t\t        try_files $uri $uri/ /index.php?$args;\n
-\t    }\n
-\n
-\t    # PHP\n
-\t    location ~ \.php$ {\n
-\t        fastcgi_buffer_size 128k;\n
-\t        fastcgi_buffers 32 32k;\n
-\t        try_files $uri =404;\n
-\t        fastcgi_pass   unix:/var/run/php5-fpm.sock;\n
-\t        fastcgi_index  index.php;\n
-\t        fastcgi_param  SCRIPT_FILENAME  $document_root$fastcgi_script_name;\n
-\t        fastcgi_param PATH_INFO $fastcgi_script_name;\n
-\t        include fastcgi_params;\n
-\t    }\n
-\n
-}\n
+mkdir -p $webdir/log
+mkdir -p $webdir/www
+mkdir -p $webdir/config
+mkdir -p $webdir/data
+touch $webdir/config/www.conf
+
+echo "网站配置文件创建中..."
+
+echo 'server {\n' >> $webdir/config/www.conf
+echo '\t    listen 80 default_server;\n' >> $webdir/config/www.conf
+echo '\t    listen [::]:80 default_server ipv6only=on;\n' >> $webdir/config/www.conf
+echo '\t    root /var/multrix;\n' >> $webdir/config/www.conf
+echo '\t    index index.html index.htm index.php;\n' >> $webdir/config/www.conf
+echo '\t    # Make site accessible from http://localhost/' >> $webdir/config/www.conf
+echo '\t    server_name localhost;\n' >> $webdir/config/www.conf
+echo '\t    location / {\n' >> $webdir/config/www.conf
+echo '\t        # 如果找不到真实存在的文件，把请求分发至 index.php' >> $webdir/config/www.conf
+echo '\t\t        try_files $uri $uri/ /index.php?$args;\n' >> $webdir/config/www.conf
+echo '\t    }\n' >> $webdir/config/www.conf
+echo '\t    # PHP\n' >> $webdir/config/www.conf
+echo '\t    location ~ \.php$ {\n' >> $webdir/config/www.conf
+echo '\t        fastcgi_buffer_size 128k;\n' >> $webdir/config/www.conf
+echo '\t        fastcgi_buffers 32 32k;\n' >> $webdir/config/www.conf
+echo '\t        try_files $uri =404;\n' >> $webdir/config/www.conf
+echo '\t        fastcgi_pass   unix:/var/run/php5-fpm.sock;\n' >> $webdir/config/www.conf
+echo '\t        fastcgi_index  index.php;\n' >> $webdir/config/www.conf
+echo '\t        fastcgi_param  SCRIPT_FILENAME  $document_root$fastcgi_script_name;\n' >> $webdir/config/www.conf
+echo '\t        fastcgi_param PATH_INFO $fastcgi_script_name;\n' >> $webdir/config/www.conf
+echo '\t        include fastcgi_params;\n' >> $webdir/config/www.conf
+echo '\t    	}\n' >> $webdir/config/www.conf
+echo '}\n' >> $webdir/config/www.conf
+
+touch $webdir/www/index.php
+echo "网站配置文件创建完成..."
+echo "<?php echo phpinfo(); ?>" > $webdir/www/index.php
+
+echo "生成启动脚本..."
+starbs=`cat << EOF
+#!/usr/bin/env bash \n
+docker stop $(docker ps -a) \n
+docker rm $(docker ps -a)\n
+docker run -d --name ng -v $(pwd)/www:/var/multrix -v $(pwd)/config:/etc/nginx/sites-enabled -v $(pwd)/data:/var/lib/mysql -p 3306:3306  -p 80:80 jackluo/ng \n
+docker exec -it ng /bin/bash \n
 \n
 EOF
 `
-echo $default > test/config/www.conf
-touch test/www/index.php
-echo "<?php echo phpinfo(); ?>" > test/www/index.php
+touch $webdir/start.sh
+echo '#!/usr/bin/env bash \n' > $webdir/start.sh
+echo 'docker stop $(docker ps -a) \n' >> $webdir/start.sh
+echo 'docker rm $(docker ps -a)\n' >>  $webdir/start.sh
+echo 'docker run -d --name ng -v $(pwd)/www:/var/multrix -v $(pwd)/config:/etc/nginx/sites-enabled -v $(pwd)/data:/var/lib/mysql -p 3306:3306  -p 80:80 jackluo/ng \n' >> $webdir/start.sh
+echo 'docker exec -it ng /bin/bash \n' >> $webdir/start.sh
+
+echo "生成启动脚本完成..."
+
+
+
+
+
+
 
 
 
